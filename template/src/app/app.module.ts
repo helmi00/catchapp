a40b1b7c7b9cbc541b6b6a5dab8ac16516bac37b
Unknown <gharsallihelmi@gmail.com>
2019-05-03 09:12:35 +0100

    update

--- template/src/app/app.module.ts
+++ template/src/app/app.module.ts
@@ -6,9 +6,21 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
-
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabase} from 'angularfire2/database';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+
+
+const config = {
+    apiKey: 'AIzaSyBu8uY-tqL5C0aKRZF0yxlnb4EZecdLHZE',
+    authDomain: 'my-project-1556121986761.firebaseapp.com',
+    databaseURL: 'https://my-project-1556121986761.firebaseio.com',
+    projectId: 'my-project-1556121986761',
+    storageBucket: 'my-project-1556121986761.appspot.com',
+    messagingSenderId: '212856553178'
+};
+// firebase.initializeApp(config);
 
 @NgModule({
     declarations: [
